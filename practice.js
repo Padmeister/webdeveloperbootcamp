80465e91e5eba395673b89df312dd1a18a8b8ce2
Paddy Bradley <patrickbradley@blueyonder.co.uk>
2017-07-24 15:21:50 +0100

    Adding files

---
+++ practice.js
@@ -0,0 +1,39 @@
+//Creates a single line comment in JS
+/*
+Creates a block comment
+*/
+
+//creates a pop up alert window
+alert("Hi there!");
+//Writes directly to the webpage contents
+document.write("Wassssup!");
+
+//Variables
+/*
+var name ="Paddy";
+var dog;
+dog = "Harry";
+
+//Data types
+
+var 1 = "String";
+var 2 = 2;
+var 3 = ["This","is","an","aray"];
+//object
+var 4 = {name:"Paddy",age:42,sex:"male"}
+*/
+
+//User created functions
+
+function funtionName(){
+	document.write("Clicked");
+}
+//to call a function
+
+//funtionName();
+
+//To call a function on an event
+//EXAMPLE
+//<p onclick="funtionName()" >Click me</p>
+var date = new Date()
+document.getElementById("test").innerHTML = date;
