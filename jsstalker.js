61876f1a60767d87d0b267a398a924557dade6ca
Paddy Bradley <patrickbradley@blueyonder.co.uk>
2017-07-25 12:59:23 +0100

    new files

---
+++ jsstalker.js
@@ -0,0 +1,9 @@
+var firstName =prompt("What is your first name?");
+var secondName=prompt("What is your second name?");
+var age=prompt("What is your age?");
+
+console.log("Your full name is " + firstName + "       "+ secondName);
+console.log("and your age is " + age);
+
+alert("Your full name is " + firstName + secondName);
+alert("and your age is "+ age);
